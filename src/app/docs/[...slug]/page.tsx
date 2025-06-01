2bc136016a81255beacf7fc8f5cbae85b7aa2042
parvajio <mdparvajmosharof17@gmail.com>
2025-06-02 03:10:47 +0600

    catch all sagment

---
+++ src/app/docs/[...slug]/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const page = async ({ params }: { params: Promise<{ slug: string[] }> }) => {
+    const { slug } = await params;
+
+    if(slug.length === 1){
+        return (
+            <div>viewing docs for fearture{slug[0]}</div>
+        )
+    }else if(slug.length === 2){
+        return(
+            <div>viewing docs for feature {slug[0]} and sub-feature {slug[1]}</div>
+        )
+    }
+    return (
+        <div>page</div>
+    )
+}
+
+export default page
