86918f2c31796073ebefcdb7c70f6e77736fdeef
parvajio <mdparvajmosharof17@gmail.com>
2025-06-02 16:38:59 +0600

    Multiple root layout

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,8 @@
+export default function AuthLayout({children}: {children: React.ReactNode}){
+    return(
+        <>
+            <h1>Authentication</h1>
+            {children}
+        </>
+    )
+}
