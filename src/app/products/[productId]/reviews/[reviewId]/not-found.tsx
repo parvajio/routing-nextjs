3334009b56d81d369ef98acf816ae6ff275c6636
parvajio <mdparvajmosharof17@gmail.com>
2025-06-02 09:09:22 +0600

    show not found page dynamically by pathname

--- src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -1,10 +1,17 @@
+"use client"
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
 const found = () => {
 
+  const pathname = usePathname();
+  const productId = pathname?.split('/')[2];
+  const reviewId = pathname?.split('/')[4];
+
+
   return (
     <div>
-        <h1>Review Not Found</h1>
+      <h1>{reviewId} no. Review Not Found in {productId} product Id </h1>
     </div>
   )
 }
