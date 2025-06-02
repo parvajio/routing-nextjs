c0eb04c04a98fb218e4334527ab0fbe914e3b956
parvajio <mdparvajmosharof17@gmail.com>
2025-06-02 08:58:08 +0600

    triggering 404 programmatically

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { notFound } from 'next/navigation';
 
-const page = async({params}:{params: Promise<{productId: string; reviewId: string}>}) => {
-    const {productId, reviewId} = await params;
+const page = async ({ params }: { params: Promise<{ productId: string; reviewId: string }> }) => {
+  const { productId, reviewId } = await params;
+
+  if(parseInt(reviewId) > 1000){
+    notFound();
+  }
+
   return (
     <div>review {reviewId} and product {productId}</div>
   )
