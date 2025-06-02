d5d7aed22ce6bb435d75c7688f8335944833e28f
parvajio <mdparvajmosharof17@gmail.com>
2025-06-02 07:56:59 +0600

    not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const found = () => {
+  return (
+    <div>
+        <h1>Page Not Found</h1>
+        <p>Could not find requested resource</p>
+    </div>
+  )
+}
+
+export default found
