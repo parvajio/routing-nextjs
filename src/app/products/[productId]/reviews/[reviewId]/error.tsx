d57689dbf2e945a0e87e76a7ca2895e44a649f88
parvajio <mdparvajmosharof17@gmail.com>
2025-06-27 05:04:31 +0600

    errro handling

---
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -0,0 +1,5 @@
+"use client"
+
+export default function ErrorBoundaay(){
+    return <h1 className="text-red-500 font-bold">Error in review page</h1>
+}
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
 import { notFound } from 'next/navigation';
 
+const getRandomInt = (count: number) =>{
+  return Math.floor(Math.random() * count)
+}
+
 const page = async ({ params }: { params: Promise<{ productId: string; reviewId: string }> }) => {
+
+  const randm = getRandomInt(2);
+  if(randm == 1){
+    throw new Error("Something went wrong");
+  }
+
   const { productId, reviewId } = await params;
 
   if(parseInt(reviewId) > 1000){
