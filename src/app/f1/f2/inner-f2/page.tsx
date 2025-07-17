61d16ceb6b74b6c5a84a49c520606266514f7dee
parvajio <mdparvajmosharof17@gmail.com>
2025-07-17 18:59:27 +0600

    intercepting routes

---
+++ src/app/f1/f2/inner-f2/page.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+const InnerF2 = () => {
+  return (
+    <>
+    <div>Inner F2</div>
+    <Link href={"/f5"}>f5</Link>
+    </>
+  )
+}
+
+export default InnerF2
---
+++ src/app/f5/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const F5 = () => {
+  return (
+    <>
+    <div>F5</div>
+    </>
+  )
+}
+
+export default F5
