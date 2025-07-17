7ee01077ad5c722d95e7f4591612db0e8294c4c4
parvajio <mdparvajmosharof17@gmail.com>
2025-07-17 18:24:49 +0600

    intercepting routes

---
+++ src/app/f1/f2/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const F2Page = () => {
+  return (
+    <div>F2 Page</div>
+  )
+}
+
+export default F2Page
---
+++ src/app/f1/page.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+const F1Page = () => {
+    return (
+        <>
+            <div>F1 Page</div>
+            <Link href={"/f1/f2"}>f2</Link>
+        </>
+    )
+}
+ 
+export default F1Page
